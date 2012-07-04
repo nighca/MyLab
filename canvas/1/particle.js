2df3b3042a6e84da9422df06a235c3f6b977f621
Hanxing Yang <nighca@live.cn>
2012-07-05 00:49:38 +0800

    simulate vector & particle system

---
+++ canvas/1/particle.js
@@ -0,0 +1,11 @@
+//particle.js
+
+Particle = function(position, velocity, life, color, size){
+	this.position = position;
+	this.velocity = velocity;
+	this.acceleration = Vector2.zero;
+	this.age = 0;
+	this.life = life;
+	this.color = color;
+	this.size = size;
+};
