ffa7b8c6bece33a24cb8200f7849325fbef20728
Hanxing Yang <nighca@live.cn>
2012-07-04 22:54:10 +0800

    init

---
+++ canvas/1/vector2.js
@@ -0,0 +1,15 @@
+﻿//vector2.js
+
+Vector2 = function(x, y) {this.x=x; this.y=y;};
+
+Vector2.prototype = {
+	copy : function(){
+		return new Vector2(this.x, this.y);
+	},
+	length : function(){
+		return Math.sqrt(this.x*this.x + this.y*this.y);
+	},
+	sqrLength : function(){
+		return this.x*this.x + this.y*this.y;
+	},
+}
