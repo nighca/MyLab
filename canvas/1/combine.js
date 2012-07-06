6bf582ceb10b49142c466ee53baf639c501f9a29
Hanxing Yang <nighca@live.cn>
2012-07-07 04:28:20 +0800

    add effector combine

---
+++ canvas/1/combine.js
@@ -0,0 +1,23 @@
+//combine of particles with each other
+
+function Combine(){
+	this.apply = function(particles){
+		
+		for (var i in particles){
+			for (var j in particles){
+				//console.log(particles[i].position.x,",",particles[i].position.y,":",particles[j].position.x,",",particles[j].position.y,"!",particles[i].dist(particles[j]));
+				if(i != j && particles[i].dist(particles[j]) <= (particles[i].size+particles[j].size)){
+					
+					particles[i].size = Math.sqrt(particles[i].size*particles[i].size + particles[j].size*particles[j].size);
+					particles[i].position = particles[i].position.add(particles[j].position).divide(2);
+					//if(particles[i].age > particles[j].age)
+					//	particles[i].age = particles[j].age;
+					if (particles.length > 1)
+			            particles[j] = particles[particles.length - 1];
+			        particles.pop();
+			        //document.write("!");
+				}
+			}
+		}
+	}
+}
--- canvas/1/main.js
+++ canvas/1/main.js
@@ -1,58 +1,78 @@
 ﻿//javascript
 var canvas, ctx, isContinue, timeoutID;
 
-function start(canvasName, func){
-	if(timeoutID) stop;
+function start(canvasName, func) {
+    if (timeoutID)
+        stop();
 
-	canvas = document.getElementById(canvasName);
-	ctx = canvas.getContext("2d");
-	isContinue = true;
+    canvas = document.getElementById(canvasName);
+    ctx = canvas.getContext("2d");
+    isContinue = true;
 
-	var loop = function(){
-		func();
-		if(isContinue)
-			timeoutID = setTimeout(loop, 10);
-	}
+    var loop = function() {
+        func();
+        if (isContinue)
+            timeoutID = setTimeout(loop, 20);
+    }
 
-
-
-	loop();	
+    loop();
 }
 
-function stop(){
-	clearTimeout(timeoutID);
-	isContinue = false;
+function stop() {
+    clearTimeout(timeoutID);
+    isContinue = false;
 }
 
-function clearCanvas(){
-	if(ctx != null)
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
+function clearCanvas() {
+    if (ctx != null)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-
 /*
 ----------------------main----------------
 */
 
-var position = new Vector2(10, 200);
-var velocity = new Vector2(50, -50);
-var acceleration = new Vector2(0, 10);
-var dt = 0.1;
+var ps = new ParticleSystem();
+ps.particleEffectors.push(new ChamberBox(0, 0, 400, 400));
+ps.systemEffectors.push(new Combine());
+var dt = 0.01;
+var i = 0;
 
-function step(){
-	position = position.add(velocity.multiply(dt));
-	velocity = velocity.add(acceleration.multiply(dt));
+function sampleDirection(angle1, angle2) {
+    var t = Math.random();
+    var theta = angle1 * t + angle2 * (1 - t);
+    return new Vector2(Math.cos(theta), Math.sin(theta));
+}
 
-	ctx.strokeStyle = "#000";
-	ctx.fillStyle = "#fff";
-	ctx.beginPath();
-	ctx.arc(position.x, position.y, 5, 0, Math.PI*2, true);
-	ctx.closePath();
-	ctx.fill();
-	ctx.stroke();
+function sampleColor(color1, color2) {
+    var t = Math.random();
+    return color1.multiply(t).add(color2.multiply(1 - t));
+}
 
-	 
+function sampleSize(maxSize){
+	return maxSize * Math.random();
+}
+
+function sampleLife(maxLife){
+	return maxLife * Math.random();
+}
+
+function step() {
+	i++;
+	if(i%40 == 0)
+    	ps.emit(
+    		new Particle(new Vector2(sampleSize(400), sampleSize(400)), 
+	    		sampleDirection(Math.PI * 0, Math.PI * 2).multiply(500), 
+	    		sampleLife(5), 
+	    		sampleColor(Color.white, Color.black), 
+	    		sampleSize(50)));
+    ps.simulate(dt);
+
+    ctx.fillStyle="rgba(0, 0, 0, 0.6)";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ps.render(ctx);
 }
 
 start("mycanvas", step);
+
 
 //step();
