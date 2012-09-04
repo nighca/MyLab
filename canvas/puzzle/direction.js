6ab20ec76ca56975f3ada5a44002c6879ac7e553
Hanxing Yang <nighca@live.cn>
2012-09-04 14:20:30 +0800

    change page

---
+++ canvas/puzzle/direction.js
@@ -0,0 +1,8 @@
+//direction.js
+
+Direction = function(x, y) {this.x=x; this.y=y;};
+
+Direction.rHoriz = new Direction(1, 0);
+Direction.lHoriz = new Direction(-1, 0);
+
+
---
+++ canvas/webGL/main.js
@@ -0,0 +1,19 @@
+﻿var triangleVertexPositionBuffer;
+var squareVertexPositionBuffer;
+
+function webGLStart(){
+    var canvas = document.getElementById("mycanvas");
+    initGL(canvas);
+    initShaders();
+    initBuffers();
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    drawscene();
+}
+
+function initBuffers() {
+    triangleVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+}
